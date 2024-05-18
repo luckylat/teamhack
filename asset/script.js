0f6e19a689748055770baab62aac349614956545
luckylat <lacklat.key@gmail.com>
2024-05-19 02:12:09 +0900

    Angularをインストール XSSテスト

--- asset/script.js
+++ asset/script.js
@@ -2,13 +2,13 @@ console.log("test");
 
 console.log(window.abc)
 
-Array.prototype.indexOf = function(){
-  console.log("overwrite array prototype")
-  return 1337;
-}
-
-Function.prototype._call = Function.prototype.call;
-Function.prototype.call = function(...args) {
-  console.log("overwrite call prototype");
-  return this._call(...args);
-}
+//Array.prototype.indexOf = function(){
+//  console.log("overwrite array prototype")
+//  return 1337;
+//}
+//
+//Function.prototype._call = Function.prototype.call;
+//Function.prototype.call = function(...args) {
+//  console.log("overwrite call prototype");
+//  return this._call(...args);
+//}
